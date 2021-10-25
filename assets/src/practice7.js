a70ae7e8522810f64855e2e49b2d9f3034084223
manhngo-dn <ngomanh.fot.hou@gmail.com>
2021-10-25 23:17:19 +0700

    update practice7

--- assets/src/practice7.js
+++ assets/src/practice7.js
@@ -3,17 +3,11 @@ let myNumber = prompt("Please input a number >=3");
 if (myNumber < 3) {
   alert("Please retry again!");
 } else {
-  let line1 = "*";
-  let line = "";
-  let space1 = "&nbsp";
-  let space = "";
-
-  for (let i = 1; i <= myNumber; i++) {
-    line = line.concat(line1);
-    document.write(line + "<br />")
-  }
-  for (let i = myNumber-1; i >= 0; i--) {
-    space = space.concat(space1);
-    document.write(space + line + "<br />")
+  for (let i = 0; i < myNumber; i++) {
+    for (let j = 0; j <= myNumber; j++) {
+      if (j < myNumber - i) document.writeln("&nbsp;&nbsp;");
+      else document.writeln(" * ");
+    }
+    document.writeln("<br/>");
   }
 }
