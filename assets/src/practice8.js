6978205a3ef28f07c5fbe6fa637e12306d37fb3c
Manh Ngo <ngomanh.fot.hou@gmail.com>
2021-10-29 23:02:37 +0700

    update practice8

--- assets/src/practice8.js
+++ assets/src/practice8.js
@@ -5,27 +5,11 @@ let gender = prompt("Input your gender");
 let school = prompt("Input your school");
 let address = prompt("Input your address");
 
-if (fname.length < 4 && lname.length < 4 && address.length >=40) {
-  alert("Input your first name/last name/address again");
-} else {
-  document.write(
-    `<table>
-      <tr>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Age</th>
-        <th>Gender</th>
-        <th>School</th>
-        <th>Address</th>
-      </tr>
-      <tr>
-        <td>${fname}</td>
-        <td>${lname}</td>
-        <td>${age}</td>
-        <td>${gender}</td>
-        <td>${school}</td>
-        <td>${address}</td>
-      </tr>
-    </table>`
-  )
+let infor = [fname, lname, age, gender, school, address];
+
+let input = document.getElementsByTagName("td");
+
+
+for (let i = 0; i < infor.length; i++) {
+  input[i].innerHTML = infor[i];
 }
